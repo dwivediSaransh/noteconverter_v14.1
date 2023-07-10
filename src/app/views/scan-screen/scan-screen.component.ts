61a65516ce70c581c48c523c4303355d8f273f3e
Aakash Jain Mphasis <akashjain.jain6@gmail.com>
2023-07-10 12:56:11 +0530

    test

--- src/app/views/scan-screen/scan-screen.component.ts
+++ src/app/views/scan-screen/scan-screen.component.ts
@@ -90,7 +90,7 @@ export class ScanScreenComponent implements OnInit{
 
   isEmailInvalid : boolean = false;
   isEmailRequired : boolean =false;
-  showEmailPlaceHolder : boolean = true;
+  showEmailPlaceHolder : boolean = false;
 
   commonRightMarginForPopup: string = '57px';
 
